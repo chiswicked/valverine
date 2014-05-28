56adc29e8f73afe8d9f96db8698890bad4005211
chiswicked <mr.norbert.metz@googlemail.com>
2014-05-28 18:51:29 +0100

    Probably not too Thread safe initial tests for length of delays

--- src/test/java/org/chiswicked/code/valverine/valve/DelayValveTest.java
+++ src/test/java/org/chiswicked/code/valverine/valve/DelayValveTest.java
@@ -1,6 +1,5 @@
 package org.chiswicked.code.valverine.valve;
 
-import org.apache.catalina.Valve;
 import org.apache.catalina.connector.Request;
 import org.apache.catalina.connector.Response;
 import org.apache.catalina.core.StandardEngine;
@@ -12,36 +11,21 @@ import org.junit.Test;
 import javax.servlet.ServletException;
 import java.io.IOException;
 
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
-
 
 public class DelayValveTest {
     private TerminatorValve terminatorValve;
-
-    private DelayValve delayValveBelowMin;
-    private DelayValve delayValveWithinRange;
-    private DelayValve delayValveAboveMax;
-
+    private DelayValve delayValve;
     private Request requestMock;
     private Response responseMock;
 
     @Before
     public void setUp() throws Exception {
         terminatorValve = mock(TerminatorValve.class);
-
-        delayValveBelowMin = new DelayValve();
-        delayValveWithinRange = new DelayValve();
-        delayValveAboveMax = new DelayValve();
-
-        delayValveBelowMin.setContainer(new StandardEngine());
-        delayValveWithinRange.setContainer(new StandardEngine());
-        delayValveAboveMax.setContainer(new StandardEngine());
-
-        delayValveBelowMin.setNext(terminatorValve);
-
-        delayValveBelowMin.setDelay(-1000);
-        delayValveWithinRange.setDelay(2000);
-        delayValveAboveMax.setDelay(50000);
+        delayValve = new DelayValve();
+        delayValve.setContainer(new StandardEngine());
+        delayValve.setNext(terminatorValve);
 
         requestMock = mock(Request.class);
         responseMock = mock(Response.class);
@@ -53,27 +37,75 @@ public class DelayValveTest {
     }
 
     @Test
-    public void testInvokeBelowMinDelay() throws Exception {
-        doRequest(delayValveBelowMin);
-    }
-/*
+    public void testInvokeBelowMinDelay5000() throws Exception {
 
-    @Test
-    public void testInvokeWithinRange() throws Exception {
-        doRequest(delayValveWithinRange);
+        int elapsedTime = doDelayedRequest(delayValve, -5000);
+
+        assertTrue(elapsedTime >= 0 && elapsedTime < 100);
     }
 
     @Test
-    public void testInvokeAboveMaxDelay() throws Exception {
-        doRequest(delayValveAboveMax);
-    }
-*/
+    public void testInvokeBelowMinDelay1000() throws Exception {
 
-    private void doRequest(Valve valveToTestWith) throws Exception {
+        int elapsedTime = doDelayedRequest(delayValve, -1000);
+
+        assertTrue(elapsedTime >= 0 && elapsedTime < 100);
+    }
+
+    @Test
+    public void testInvokeBelowMinDelay1() throws Exception {
+
+        int elapsedTime = doDelayedRequest(delayValve, -1);
+
+        assertTrue(elapsedTime >= 0 && elapsedTime < 100);
+    }
+
+    @Test
+    public void testInvokeWithinRange0() throws Exception {
+
+        int elapsedTime = doDelayedRequest(delayValve, 0);
+
+        assertTrue(elapsedTime >= 0 && elapsedTime < 100);
+    }
+
+    @Test
+    public void testInvokeWithinRange100() throws Exception {
+
+        int elapsedTime = doDelayedRequest(delayValve, 100);
+
+        assertTrue(elapsedTime >= 50 && elapsedTime < 150);
+    }
+
+    @Test
+    public void testInvokeWithinRange1000() throws Exception {
+
+        int elapsedTime = doDelayedRequest(delayValve, 1000);
+
+        assertTrue(elapsedTime >= 950 && elapsedTime < 1050);
+    }
+
+    @Test
+    public void testInvokeAboveMaxDelay31000() throws Exception {
+        int elapsedTime = doDelayedRequest(delayValve, 1000);
+
+        assertTrue(elapsedTime >= 950 && elapsedTime < 1050);
+    }
+
+
+    private int doDelayedRequest(DelayValve valveToTestWith, int delay) throws Exception {
+        valveToTestWith.setDelay(delay);
+
+        long startTime = System.currentTimeMillis();
+
         valveToTestWith.invoke(requestMock, responseMock);
+
+        long endTime = System.currentTimeMillis();
+
+        return (int) (endTime - startTime);
     }
 
     private class TerminatorValve extends ValveBase {
+
         @Override
         public void invoke(Request request, Response response) throws IOException, ServletException {
 
