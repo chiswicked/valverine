ebe4d4b97c4076f35ce7be47fb40e4ef49224e15
chiswicked <mr.norbert.metz@googlemail.com>
2014-05-28 02:24:34 +0100

    Added mock Container context for testing Valve behaviour

--- src/test/java/org/chiswicked/code/valverine/valve/DelayValveTest.java
+++ src/test/java/org/chiswicked/code/valverine/valve/DelayValveTest.java
@@ -1,14 +1,50 @@
 package org.chiswicked.code.valverine.valve;
 
+import org.apache.catalina.Valve;
+import org.apache.catalina.connector.Request;
+import org.apache.catalina.connector.Response;
+import org.apache.catalina.core.StandardEngine;
+import org.apache.catalina.valves.ValveBase;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+import static org.mockito.Mockito.mock;
+
+
 public class DelayValveTest {
+    private TerminatorValve terminatorValve;
+
+    private DelayValve delayValveBelowMin;
+    private DelayValve delayValveWithinRange;
+    private DelayValve delayValveAboveMax;
+
+    private Request requestMock;
+    private Response responseMock;
 
     @Before
     public void setUp() throws Exception {
+        terminatorValve = mock(TerminatorValve.class);
 
+        delayValveBelowMin = new DelayValve();
+        delayValveWithinRange = new DelayValve();
+        delayValveAboveMax = new DelayValve();
+
+        delayValveBelowMin.setContainer(new StandardEngine());
+        delayValveWithinRange.setContainer(new StandardEngine());
+        delayValveAboveMax.setContainer(new StandardEngine());
+
+        delayValveBelowMin.setNext(terminatorValve);
+
+        delayValveBelowMin.setDelay(-1000);
+        delayValveWithinRange.setDelay(2000);
+        delayValveAboveMax.setDelay(50000);
+
+        requestMock = mock(Request.class);
+        responseMock = mock(Response.class);
     }
 
     @After
@@ -17,7 +53,30 @@ public class DelayValveTest {
     }
 
     @Test
-    public void testInvoke() throws Exception {
+    public void testInvokeBelowMinDelay() throws Exception {
+        doRequest(delayValveBelowMin);
+    }
+/*
 
+    @Test
+    public void testInvokeWithinRange() throws Exception {
+        doRequest(delayValveWithinRange);
+    }
+
+    @Test
+    public void testInvokeAboveMaxDelay() throws Exception {
+        doRequest(delayValveAboveMax);
+    }
+*/
+
+    private void doRequest(Valve valveToTestWith) throws Exception {
+        valveToTestWith.invoke(requestMock, responseMock);
+    }
+
+    private class TerminatorValve extends ValveBase {
+        @Override
+        public void invoke(Request request, Response response) throws IOException, ServletException {
+
+        }
     }
 }
